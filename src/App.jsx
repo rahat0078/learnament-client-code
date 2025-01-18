f2df45431884d181d4ebd94c192f2e6121a9ecea
rahat0078 <rarahat1710@gmail.com>
2025-01-18 17:42:03 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-4xl text-center">Welcome to LearnaMent</h1>
+      <button className="btn btn-info text-white">See the update</button>
+    </>
+  )
+}
+
+export default App
